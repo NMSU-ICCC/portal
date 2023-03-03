e51ebd70bbff03b6877364c3aea0bf38201f1446
Manuel Mares <manuels@nmsu.edu>
2023-03-03 11:38:44 -0700

    addec content to main. Added routes

--- index.js
+++ index.js
@@ -1,16 +1,94 @@
-const files = [
-    "pages/navMenu.html",
-    "pages/main.html",
-]
+const pagesfiles = {
+    home : [
+        "pages/main.html",
+    ],
+    aboutUs : [
+        "pages/navMenu.html",
+        "pages/aboutUs.html"
+    ],
+    documents : [
+        "pages/navMenu.html",
+        
+    ],
+    projects : [
+        "pages/navMenu.html",
+        
+    ],
+    meetings : [
+        "pages/navMenu.html",
+        
+    ]
+}
 
 
-async function insertHTMLfiles() {
-    files.forEach(async e => {
+//This function gets trigger when the page already loaded so it doesn't fail
+//it adds the navBar and then the home page
+window.addEventListener('load', function () {    
+    loadNavBar();
+    addHMLTContent(pagesfiles["home"]);
+});
+
+function loadNavBar(){
+    addHMLTContent(["pages/navMenu.html"]);
+    createNavBar_addEvenetListeners();
+}
+function createNavBar_addEvenetListeners(){
+    waitForElement('.TopMenu').then((elm) => {
+        document.getElementById("topMenu_optionButton_Home"       ).onclick = function(){     loadRoute(pagesfiles["home"]);    }
+        document.getElementById("topMenu_optionButton_AboutUs"    ).onclick = function(){     loadRoute(pagesfiles["aboutUs"]); }
+        document.getElementById("topMenu_optionButton_Meetings"   ).onclick = function(){     loadRoute(pagesfiles["home"]);    }
+        document.getElementById("topMenu_optionButton_Documents"  ).onclick = function(){     loadRoute(pagesfiles["home"]);    }
+        document.getElementById("topMenu_optionButton_Projects"   ).onclick = function(){     loadRoute(pagesfiles["home"]);    }
+        document.getElementById("topMenu_optionButton_Gallery"    ).onclick = function(){     loadRoute(pagesfiles["home"]);    }
+    });
+}
+function waitForElement(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                resolve(document.querySelector(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+
+
+
+//every time we go to a different section
+function loadRoute(filesToAdd){
+    deleteHTMLContent();
+    loadNavBar();
+    addHMLTContent(filesToAdd);
+}
+function deleteHTMLContent(){
+    document.body.innerHTML = "";
+}
+
+
+
+
+
+
+
+//add the indicated HTML files into the page
+async function addHMLTContent(content) {
+    content.forEach(async e => {
         htmlNode = await bringHTMLFile(e);
         document.body.insertAdjacentHTML("beforeend", htmlNode);
     })
 }
-
+//retrieves and parses an HTML file
 async function bringHTMLFile(path){
     const resp = await fetch(path);
     file = await resp.text();
@@ -19,4 +97,3 @@ async function bringHTMLFile(path){
 
 
 
-insertHTMLfiles();
